cbab28cfa74122750e0b2bd7de8c215702fb2a5f
digitalrelic <daniel.rankin@me.com>
2015-09-22 20:10:33 -0600

    Final revision
    app icons, launch screen, other layout tweaks

--- Lab 2/Lab 2/ViewController.swift
+++ Lab 2/Lab 2/ViewController.swift
@@ -19,37 +19,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var baconView: UIImageView!
     @IBOutlet weak var cookSlider: UISlider!
     @IBOutlet weak var baconLabel: UILabel!
-    @IBOutlet weak var instructions: UILabel!
-    @IBOutlet weak var segmentedButton: UISegmentedControl!
+    @IBOutlet weak var switchControl: UISegmentedControl!
+    @IBOutlet weak var instLabel: UILabel!
+    @IBOutlet weak var inst: UILabel!
 
+    func showInstructions(){
+        if switchControl.selectedSegmentIndex == 0 {
+            instLabel.text="Use slider to cook bacon."
+           
+        }
+        else if switchControl.selectedSegmentIndex == 1 {
+            instLabel.text=" "
 
-//    @IBAction func instructionButton(sender:UISegmentedControl) {
-//        if sender.selectedSegmentIndex == 0 {
-//            instructions.text = "Drag slider to cook bacon!"
-//        }
-//        else if sender.selectedSegmentIndex == 1 {
-//            instructions.text = " "
-//        }
-//    }
+        }
+    }
+    
+    @IBAction func changeText(sender: UISegmentedControl) {
+        showInstructions()
+    }
+    
+    
     
     
 
     @IBAction func baconSlider(sender: UISlider) {
         var currentValue = Int(sender.value)
-        
         if currentValue == 0 {
+            baconView.image=UIImage(named: "packaged")
+            baconLabel.text = "packaged"
+        }
+        else if currentValue == 1 {
             baconView.image=UIImage(named: "rawbacon")
             baconLabel.text = "raw"
         }
-        else if currentValue == 1{
+        else if currentValue == 2{
             baconView.image=UIImage(named: "bacon_1")
             baconLabel.text = "medium raw"
         }
-        else if currentValue == 2{
+        else if currentValue == 3{
             baconView.image=UIImage(named: "bacon_2")
             baconLabel.text = "medium"
         }
-        else if currentValue == 3{
+        else if currentValue == 4{
             baconView.image=UIImage(named: "bacon_3")
             baconLabel.text = "burnt"
         }
